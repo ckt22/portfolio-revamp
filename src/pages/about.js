041842dbd2a1b84b035cf59f4c89f3f592925d79
ckt22 <cktangad@connect.ust.hk>
2022-05-16 22:56:11 +0800

    cv update

--- src/pages/about.js
+++ src/pages/about.js
@@ -25,19 +25,24 @@ const AboutPage = ({ data }, location) => {
               fluid={data.benchAccounting.childImageSharp.fluid}
               className="kg-image"
             />
-            <figcaption>Finally graduated and I'm feeling so guuddddddd:D</figcaption>
+            <figcaption>
+              Finally graduated and I'm feeling so guuddddddd:D
+            </figcaption>
           </figure>
           <h3 id="dynamic-styles">Name's Kit.</h3>
           <p>
-            I am a fresh graduate from HKUST, class of 2021. My major is Computer Engineering (CPEG), and I am also doing a business minor.
+            I am a fresh graduate from HKUST, class of 2021. My major is
+            Computer Engineering (CPEG), and I am also doing a business minor.
           </p>
           <p>
-            I am a big fan of coffee and fresh air. I like to hike and sometimes going to the gym. And I always welcome casual hangouts.
+            I am a big fan of coffee and fresh air. I like to hike and sometimes
+            going to the gym. And I always welcome casual hangouts.
           </p>
           <p>
-            Experienced in full-stack web development, however, I am also eager to explore beyond my strengths.
-            I am open for various opportunities, especially blockchain and FinTech.
-            I am industry focused, which makes me motivated in trying everything in the right environment.
+            Experienced in full-stack web development, however, I am also eager
+            to explore beyond my strengths. I am open for various opportunities,
+            especially blockchain and FinTech. I am industry focused, which
+            makes me motivated in trying everything in the right environment.
             {/* Both post and page templates are light and minimal, with all the
             focus on the content while the design of the theme gets out of the
             way. Beneath the hood, London enjoys the full power of the{" "}
@@ -47,19 +52,21 @@ const AboutPage = ({ data }, location) => {
             to provide limitless customisation options and dynamic styles. */}
           </p>
           <p>
-            This is where I share my previous work. Occasionally, I may also upload some blogs of technology, business insights, or even my daily life.
+            This is where I share my previous work. Occasionally, I may also
+            upload some blogs of technology, business insights, or even my daily
+            life.
             {/* Don't forget to check out the{" "}
             <a href="https://docs.ghost.org/integrations/">
               Ghost Integrations Directory
             </a>{" "}
             for more ways to integrate Ghost with your favourite services. */}
           </p>
-          <a 
+          <a
             target="_blank"
             rel="noopener noreferrer"
-            href={'/portfolio-revamp/resume.pdf'}
+            href={"/portfolio-revamp/resume.pdf"}
           >
-          <button>My CV (Jan 2022)</button>
+            <button>My CV (May 2022)</button>
           </a>
         </div>
       </article>
@@ -74,9 +81,7 @@ const indexQuery = graphql`
         title
       }
     }
-    benchAccounting: file(
-      relativePath: { eq: "me.jpg" }
-    ) {
+    benchAccounting: file(relativePath: { eq: "me.jpg" }) {
       childImageSharp {
         fluid(maxWidth: 1360) {
           ...GatsbyImageSharpFluid
